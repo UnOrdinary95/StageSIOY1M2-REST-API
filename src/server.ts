6aefa8ac524431a6a2907fd881a05ccefb3d685d
UnOrdinary <114882529+UnOrdinary95@users.noreply.github.com>
2025-11-23 18:28:24 +0100

    refactor: replace promise-based DB connection with async/await for improved readability

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import app from "./app.js";
 import { connectDb } from "./config/db.js";
 import { PORT } from "./constants.js";
 
-connectDb()
-    .then(() => app.listen(PORT, () => console.log(`Serveur est en cours d'exécution sur http://localhost:${PORT}`)))
-    .catch((err) => {
-        console.error('Erreur lors de la connexion à MongoDB', err);
-        process.exit(1);
-    });
+try {
+    await connectDb();
+    app.listen(PORT, () => console.log(`Serveur est en cours d'exécution sur http://localhost:${PORT}`));
+} catch (err) {
+    console.error('Erreur lors de la connexion à MongoDB', err);
+    process.exit(1);
+}
