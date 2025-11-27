74a84d527b44710d03ef273ebcc1d340b802697f
UnOrdinary <114882529+UnOrdinary95@users.noreply.github.com>
2025-11-27 14:24:11 +0100

    chore: improve dev environment and naming consistency
    - Add -L flag to nodemon for file watching in Docker
    - Rename MongoDB collections from lowercase to PascalCase (LightNovel, User)
    - Add documentation URL log message on server startup

--- src/config/init-db.js
+++ src/config/init-db.js
@@ -1,5 +1,5 @@
 db = db.getSiblingDB("novelya"); // Bascule sur la base de données "novelya"
 
 // Crée si ils n'existent pas déjà
-db.createCollection("lightnovels");
-db.createCollection("users");
+db.createCollection("LightNovel");
+db.createCollection("User");
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { logger } from "./utils/loggerUtils.js";
 try {
     await connectDb();
     logger.info(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
+    logger.info(`Accès à la documentation sur http://localhost:${PORT}/docs`);
     app.listen(PORT);
 } catch (err) {
     logger.error('DB Connection', err);
